bae22f261c3798ad215954e80835245d2b678d6a
akv <skyskykv@gmail.com>
2025-10-13 10:26:26 +0530

    secondcommit

--- src/components/Headersection.jsx
+++ src/components/Headersection.jsx
@@ -5,7 +5,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { FaPhone,FaPhoneAlt } from 'react-icons/fa';
 import logo from "../assets/logo.png";
-
+import { Link } from 'react-router-dom';  
 
 function Headersection() {
   return (
@@ -20,7 +20,23 @@ function Headersection() {
             <Nav.Link href="#link" className=" animate__animated animate__fadeInUp"style={{ animationDelay: "0.1s",color: "#05155dff" }}>About</Nav.Link>
             <Nav.Link href="#link" className=" animate__animated animate__fadeInUp"style={{ animationDelay: "0.1s",color: "#05155dff" }}>Blog</Nav.Link>
             <Nav.Link href="#link" className=" animate__animated animate__fadeInUp"style={{ animationDelay: "0.1s",color: "#05155dff" }}>Contact</Nav.Link>
-            <button className="custom-btn  animate__animated animate__fadeInUp"style={{ animationDelay: "0.1s",color: "#05155dff" }} ><FaPhoneAlt/> 9845624</button>
+            <button className="custom-btn d-none d-lg-inline-block animate__animated animate__fadeInUp"style={{ animationDelay: "0.1s",color: "#05155dff" }} ><FaPhoneAlt/> 9845624</button>
+             {/* Mobile button */}
+            <button
+              className="custom-btn d-lg-none animate__animated animate__fadeInUp"
+              style={{
+                animationDelay: "0.1s",
+                color: "#05155dff",
+                fontSize: "13px",
+                // padding: "6px 10px",
+                width: "20%",
+                textAlign: "center",
+                marginTop: "10px",
+                marginLeft:'-25px'
+              }}
+            >
+              <FaPhoneAlt /> 9845624
+            </button>
             {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
